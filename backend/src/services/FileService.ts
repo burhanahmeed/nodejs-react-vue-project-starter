244a26ee03d8535a162dc763b7caf928ee4f122b
Burhan Ahmed <burhanahmeed@gmail.com>
2023-09-16 09:44:22 +0700

    add service

--- backend/src/services/FileService.ts
+++ backend/src/services/FileService.ts
@@ -1,28 +1,51 @@
 import { ModelStatic, Op } from 'sequelize';
 import db from '../models';
 import { IFindOptions } from '../types/base';
+import ApiError from '../utils/apiError'
 
 export default class FileService {
   private file: ModelStatic<any>;
 
   constructor() {
-    this.file = db.User;
+    this.file = db.File;
   }
 
-  store() {
-
+  store(payload: any) {
+    return this.file.create(payload);
   }
 
-  updateById() {
+  async updateByIdIfExist(id: number, payload: any) {
+    try {
+      const file = this.getById(id);
+      if (!file) {
+        throw new ApiError(404, 'not_found', 'file_service', 'file you requested does not exist');
+      }
 
+      return this.file.update(payload, { where: { id } });
+    } catch (error) {
+      throw error;
+    }
   }
 
-  deleteById() {
-
+  getById(id: number) {
+    return this.file.findOne({ where: { id } });
   }
 
-  listAll() {
-    
+  deleteByIdIfExist(id: number) {
+    try {
+      const file = this.getById(id);
+      if (!file) {
+        throw new ApiError(404, 'not_found', 'delete_file_service', 'file you requested does not exist');
+      }
+
+      return this.file.destroy({ where: { id } });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  listAll(opts: IFindOptions) {
+    return this.file.findAll({ where: { email: { [Op.like]: `%${opts?.filters?.search}%` } } });
   }
 }
 
