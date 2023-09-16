a6f6b54f6484dd6e69a65abb1dfff258790acec3
Burhan Ahmed <burhanahmeed@gmail.com>
2023-09-16 10:00:50 +0700

    add routes

--- backend/src/controllers/FilesController.ts
+++ backend/src/controllers/FilesController.ts
@@ -1,10 +1,28 @@
 import { Request, Response, NextFunction } from 'express';
+import Validator from 'validatorjs';
 import BaseController from './BaseController';
+import { File } from "../services/FileService";
 
 export default class FilesController extends BaseController {
-  public static create(req: Request, res: Response, next: NextFunction) {
+  public static async create(req: Request, res: Response, next: NextFunction) {
     try {
-      
+      FilesController.validateForm(req.body, {
+        name: 'required'
+      });
+
+      if (!req.file) {
+        throw super.throwBadRequest('create_file', 'Image must be exist');
+      }
+
+      await File.store({
+        ...req.body,
+        image_path: req.file.filename
+      });
+
+      res.json({
+        success: true,
+        message: 'File has been stored'
+      });
     } catch (error) {
       next(error);
     }
@@ -18,19 +36,40 @@ export default class FilesController extends BaseController {
     }
   }
 
-  public static update(req: Request, res: Response, next: NextFunction) {
+  public static async update(req: Request, res: Response, next: NextFunction) {
     try {
-      
+      await File.updateByIdIfExist(Number(req.params.id), req.body);
+
+      res.json({
+        success: true,
+        message: 'File has been updated',
+        data: { id: req.params.id, name: req.body.name }
+      });
     } catch (error) {
       next(error);
     }
   }
 
-  public static delete(req: Request, res: Response, next: NextFunction) {
+  public static async delete(req: Request, res: Response, next: NextFunction) {
     try {
-      
+      await File.deleteByIdIfExist(Number(req.params.id));
+
+      res.json({
+        success: true,
+        message: 'File has been deleted',
+        data: { id: req.params.id, name: req.body.name }
+      });
     } catch (error) {
       next(error);
     }
+  }
+
+  private static validateForm(data: any, rules: any) {
+    const validation = new Validator(data, rules);
+    if (validation.fails()) {
+      super.throwFormValidationError('file_validation', validation.errors.errors);
+    }
+
+    return true;
   }
 }
---
+++ backend/src/routes/files.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import file from '../controllers/FilesController';
+import { checkUser } from '../middlewares/authentication';
+import { useRules } from '../middlewares/roles';
+import { RULES } from '../constants/rules';
+import { uploaderDiskMiddleware } from '../utils/uploadFile';
+
+const router = express.Router();
+
+// middleware check
+router.use(checkUser);
+
+router.post('/', useRules([RULES.CAN_CREATE]), uploaderDiskMiddleware.single('image'), file.create);
+router.post('/:id', useRules([RULES.CAN_CREATE, RULES.CAN_EDIT]), uploaderDiskMiddleware.single('image'), file.update);
+router.get('/', useRules([RULES.CAN_CREATE, RULES.CAN_EDIT]), file.list);
+router.delete('/:id', useRules([RULES.CAN_DELETE]), file.delete);
+
+export default router;
